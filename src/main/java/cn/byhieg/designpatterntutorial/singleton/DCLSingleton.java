b11ae9167995a2740367f9738058b115173ad7fb
byhieg <byhieg@gmail.com>
2017-03-14 11:26:39 +0800

    添加线程安全的懒汉写法的单例模式

---
+++ src/main/java/cn/byhieg/designpatterntutorial/singleton/DCLSingleton.java
@@ -0,0 +1,26 @@
+package cn.byhieg.designpatterntutorial.singleton;
+
+/**
+ * Created by shiqifeng on 2017/3/14.
+ * Mail byhieg@gmail.com
+ */
+public class DCLSingleton {
+
+    private static volatile DCLSingleton singleton;
+
+    private DCLSingleton(){
+
+    }
+
+    public  static DCLSingleton getSingleton(){
+        if (singleton == null) {
+            synchronized (DCLSingleton.class) {
+                if (singleton == null) {
+                    singleton = new DCLSingleton();
+                }
+            }
+        }
+        return singleton;
+    }
+
+}
