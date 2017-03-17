a34b33c46eb2e08316109fbeb9c0660d11959b04
byhieg <byhieg@gmail.com>
2017-03-17 11:33:19 +0800

    增加动态代理类

---
+++ src/main/java/cn/byhieg/designpatterntutorial/proxy/dynamicproxy/DynamicProxy.java
@@ -0,0 +1,18 @@
+package cn.byhieg.designpatterntutorial.proxy.dynamicproxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * Created by shiqifeng on 2017/3/17.
+ * Mail byhieg@gmail.com
+ */
+public class DynamicProxy implements InvocationHandler {
+    private Object object;
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Object result = method.invoke(object, args);
+        return result;
+    }
+}
