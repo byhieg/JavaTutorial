d200fb2c02e3ad351125a6a893dcb841e585fae6
石琦峰 <byhieg@gmail.com>
2017-05-03 17:23:12 +0800

    添加阻塞队列

---
+++ src/test/java/cn/byhieg/threadtutorialtest/concurrenttest/blockingtest/ArrayBlockTest.java
@@ -0,0 +1,34 @@
+package cn.byhieg.threadtutorialtest.concurrenttest.blockingtest;
+
+import cn.byhieg.threadtutorial.concurrent.blocking.ArrayBlock;
+import cn.byhieg.threadtutorial.concurrent.blocking.Costumer;
+import cn.byhieg.threadtutorial.concurrent.blocking.Producer;
+import junit.framework.TestCase;
+
+import javax.swing.*;
+
+/**
+ * Created by byhieg on 17/5/3.
+ * Mail to byhieg@gmail.com
+ */
+public class ArrayBlockTest extends TestCase {
+    ArrayBlock block;
+    public void setUp() throws Exception {
+        super.setUp();
+        block = new ArrayBlock();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+
+    public void testBlocking() throws Exception {
+        Producer producer = new Producer(block);
+        Costumer costumer = new Costumer(block);
+        producer.start();
+        costumer.start();
+        producer.join();
+        costumer.join();
+
+    }
+}
